7753b91bd57b52afba7314adf8e27679f9527a4f
Herbert Joseph <techniquea2z@gmail.com>
2025-10-21 14:37:55 -0400

    fix(css): navbar

--- components/ExplorerHome.jsx
+++ components/ExplorerHome.jsx
@@ -441,7 +441,7 @@ const styles = StyleSheet.create({
     ...Platform.select({
       web: {
         position: 'fixed',
-        top: 75,
+        top: 90,
         left: 0,
         right: 0,
         zIndex: 99,
