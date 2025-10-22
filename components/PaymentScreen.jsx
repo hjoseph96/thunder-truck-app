a3a07ed8c8f46f37581bbdaf8ac268514eb4633e
Herbert Joseph <techniquea2z@gmail.com>
2025-10-22 18:39:13 -0400

    fix(delivery-instructions): fix length of <textarea>

--- components/PaymentScreen.jsx
+++ components/PaymentScreen.jsx
@@ -451,6 +451,11 @@ const PaymentScreen = ({ route, navigation }) => {
                 placeholderTextColor="#999"
                 multiline
                 numberOfLines={9}
+                {...(Platform.OS === 'web' && {
+                  style: {
+                    numberOfLines: 3, // Ensure only 3 lines are visible on web
+                  },
+                })}
                 maxLength={300}
               />
             </View>
